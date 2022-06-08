a1eff92f5f1ec1209f9b46c86e5a89ea4c6407eb
Aymeric Masse <aymeric.masse@simplifield.com>
2022-06-08 16:47:28 +0200

    fat(charts): remove annotations moduile

--- src/highcharts-ng.js
+++ src/highcharts-ng.js
@@ -16,7 +16,6 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
         require('highcharts/modules/exporting')(Highcharts);
         require('highcharts/modules/offline-exporting')(Highcharts);
         require('highcharts/modules/export-data')(Highcharts);
-        require('highcharts/modules/annotations')(Highcharts);
   }
 
 
